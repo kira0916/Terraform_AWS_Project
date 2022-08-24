11e766d9fe49ec3b7e7f97df2d27d1b1bf741630
kira0916 <rjrjswm@gmail.com>
2022-08-25 02:15:48 +0900

    2022-08-25

--- install1.sh
+++ install1.sh
@@ -19,5 +19,5 @@ git init
 git add *
 git commit -m "1"
 git branch -M main
-git push --set-upstream https://x1nyub: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@github.com/x1nyub/test1.git main
-docker pull heeyoonchai/tomcat:1
+git push --set-upstream https://x1nyub:ghp@github.com/x1nyub/test1.git main
+docker pull heeyoonchai/tom:5
--- install2.sh
+++ install2.sh
@@ -5,11 +5,11 @@ amazon-linux-extras install docker -y
 amazon-linux-extras enable docker
 systemctl enable docker
 systemctl start docker
-docker pull heeyoonchai/tomcat:2
+docker pull heeyoonchai/tom:5
 yum install -y git
 sleep 10
 git clone https://github.com/x1nyub/test1.git
 cd test1
 chmod 777 test.sh
 ./test.sh
-docker service create --name tomcat -p 8009:8009 --dns=8.8.8.8 --replicas 4 heeyoonchai/tomcat:2
+docker service create --name tomcat -p 8080:8080 --dns=8.8.8.8 --replicas 4 heeyoonchai/tom:5
